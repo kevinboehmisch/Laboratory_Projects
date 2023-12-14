27d86e8f9dab915c62db2133893273188d87e077
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2023-12-14 21:49:25 +0100

    Update Manager.h

--- Object_Oriented_Systems/Laboratory_Project_13/Manager.h
+++ Object_Oriented_Systems/Laboratory_Project_13/Manager.h
@@ -1,1 +1,17 @@
+#include "Employee.h"
+
+#include <string>
+using namespace std;
+
+
+class Manager : public Employee
+{
+    double _salary;
+
+public:
+    Manager(const string, const string, double);
+    double get_income() const;
+    void print() const;
+};
+
 
