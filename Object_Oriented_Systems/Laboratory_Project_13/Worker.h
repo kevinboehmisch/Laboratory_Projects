d23d85102bbbc5327c6c7c948e634671122e2871
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2023-12-14 21:50:47 +0100

    Update Worker.h

--- Object_Oriented_Systems/Laboratory_Project_13/Worker.h
+++ Object_Oriented_Systems/Laboratory_Project_13/Worker.h
@@ -1,1 +1,20 @@
 
+
+#include "Employee.h"
+
+#include <string>
+using namespace std;
+
+
+
+class Worker : public Employee
+{
+    double _hourly_earnings;
+    double _hours;
+
+public:
+    Worker(const string, const string, double = 0.0, double = 0.0);
+    double get_income() const;
+    void print() const;
+};
+
