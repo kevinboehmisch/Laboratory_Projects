91b7d1000cde5311a3b02340812538f2e9386b6f
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2023-12-14 21:47:12 +0100

    Update Employee.cpp

--- Object_Oriented_Systems/Laboratory_Project_13/Employee.cpp
+++ Object_Oriented_Systems/Laboratory_Project_13/Employee.cpp
@@ -1,1 +1,17 @@
+#include "Employee.h"
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+Employee::Employee(const string f_n, const string l_n)
+{
+    _first_name = f_n;
+    _last_name = l_n;
+}
+void Employee::print() const
+{
+    cout << _last_name << ", " << _first_name << endl;
+}
