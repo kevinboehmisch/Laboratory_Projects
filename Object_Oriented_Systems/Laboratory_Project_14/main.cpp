80564b3f3aee9109b744e233db016368d14ec1bf
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2023-12-14 21:53:26 +0100

    Update main.cpp

--- Object_Oriented_Systems/Laboratory_Project_14/main.cpp
+++ Object_Oriented_Systems/Laboratory_Project_14/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+import empl;
+import manager;
+import worker;
+
+int main()
+{
+    Manager m("Gerd", "Mayer", 1000.00);
+    m.print();
+    cout << "Gehalt: " << m.get_income() << endl << endl;
+    Worker w("Kevin", "Böhmisch", 15.00, 40);
+    w.print();
+    cout << "Lohn: " << w.get_income() << endl;
+    return 0;
+}
