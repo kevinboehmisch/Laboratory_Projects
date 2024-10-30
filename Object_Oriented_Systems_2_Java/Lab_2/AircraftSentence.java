cab9a9608a6000b4f91289238b0df9e53a9edd7b
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2024-10-30 11:11:03 +0100

    Create AircraftSentence.java

---
+++ Object_Oriented_Systems_2_Java/Lab_2/AircraftSentence.java
@@ -0,0 +1,12 @@
+    public AircraftSentence(JSONArray aircraftData) {
+        this.aircraftData = aircraftData;
+    }
+
+    /**
+     * Diese Funktion gibt die Daten des Aircraftsentence als JSONArray zurück
+     * @return JSONArray
+     */
+    public JSONArray getAircraftSentence() {
+        return aircraftData;
+    }
+}
